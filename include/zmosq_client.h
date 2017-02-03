f33e83ff1e4a95f043dd9879567c4b91254dbb99
Karol Hrdina <karol.hrdina@gmail.com>
2017-02-03 15:23:44 +0100

    PROBLEM: There is no api for zmosq_client
    SOLUTION: Add it (even if ugly :)).
    
    Signed-off-by: Karol Hrdina <KarolHrdina@Eaton.com>

--- include/zmosq_client.h
+++ include/zmosq_client.h
@@ -26,6 +26,70 @@ ZMSQ_EXPORT zmosq_client_t *
 ZMSQ_EXPORT void
     zmosq_client_destroy (zmosq_client_t **self_p);
 
+//  Start the zmosq_client 
+ZMSQ_EXPORT void
+    zmosq_client_start (zmosq_client_t *self);
+
+//  Stop the zmosq_client  
+ZMSQ_EXPORT void
+    zmosq_client_stop (zmosq_client_t *self);
+
+//  Set verbosity   
+ZMSQ_EXPORT void
+    zmosq_client_set_verbose (zmosq_client_t *self, bool verbose);
+
+//  Is client connected to mosquitto broker?
+ZMSQ_EXPORT bool
+    zmosq_client_mqtt_connected (zmosq_client_t *self);
+
+//  Connect client to mosquitto broker
+ZMSQ_EXPORT int
+    zmosq_client_mqtt_connect (zmosq_client_t *self, const char *host, int port, int keepalive, const char *bind_address);
+
+//  Get mosquitto broker's hostname, to which client is connected   
+ZMSQ_EXPORT const char *
+    zmosq_client_host (zmosq_client_t *self);
+
+//  Get mosquitto broker's port, to which client is connected   
+ZMSQ_EXPORT int 
+    zmosq_client_port (zmosq_client_t *self);
+
+//  Get mosquitto broker's keepalive, to which client is connected   
+ZMSQ_EXPORT int 
+    zmosq_client_keepalive (zmosq_client_t *self);
+
+//  Get mosquitto broker's bindaddress, to which client is connected   
+ZMSQ_EXPORT const char *
+    zmosq_client_bindaddress (zmosq_client_t *self);
+
+//  Subscribe to mosquitto broker's topic  
+ZMSQ_EXPORT void
+    zmosq_client_subscribe (zmosq_client_t *self, const char *topic);
+
+//  Get mosquitto broker's topics to which client is subscribed
+ZMSQ_EXPORT zlistx_t *
+    zmosq_client_topics (zmosq_client_t *self, const char *topic);
+
+//  Is client connected to malamute broker  
+ZMSQ_EXPORT bool
+    zmosq_client_mlm_connected (zmosq_client_t *self);
+
+//  Connect client to malamute broker
+ZMSQ_EXPORT void
+    zmosq_client_mlm_connect (zmosq_client_t *self, const char *mlm_endpoint);
+
+//  Get malamute broker's hostname to which client is connected
+ZMSQ_EXPORT const char *
+    zmosq_client_mlm_host (zmosq_client_t *self);
+
+//  Publish to malamute broker's stream
+ZMSQ_EXPORT void
+    zmosq_client_mlm_set_stream (zmosq_client_t *self, const char *stream);
+
+//  Get malamute broker's stream to which client is publishing 
+ZMSQ_EXPORT const char *
+    zmosq_client_mlm_stream (zmosq_client_t *self);
+
 //  Self test of this class
 ZMSQ_EXPORT void
     zmosq_client_test (bool verbose);
