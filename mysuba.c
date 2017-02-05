5f7a408d63941568b69dd3a3e8205b5e1af4f9ad
Michal Vyskocil <michalvyskocil@eaton.com>
2017-02-05 18:35:21 +0100

    Problem: async mqtt loop can only print the data
    Solution: pass the messages from async mqtt thread via EXCLPAIR sockets.

--- mysuba.c
+++ mysuba.c
@@ -6,8 +6,8 @@ s_connect (struct mosquitto *mosq, void *obj, int result) {
     fprintf (stderr, "D: s_connect, result=%d\n", result);
 
     if (!result) {
-        fprintf (stderr, "D: !s_connect, !result\n");
-        int r = mosquitto_subscribe (mosq, NULL, "TEST", 0);
+        fprintf (stderr, "D: s_connect, !result\n");
+        int r = mosquitto_subscribe (mosq, NULL, "#", 0);
         assert (r == MOSQ_ERR_SUCCESS);
     }
     else
@@ -26,32 +26,37 @@ s_message (struct mosquitto *mosq, void *obj, const struct mosquitto_message *me
 
 	assert(obj);
 	cfg = (struct mosq_config *)obj;
+    zsock_t *mqtt_writter = (zsock_t*) obj;
 
-		if(message->payloadlen){
-			printf("%s ", message->topic);
-			fwrite(message->payload, 1, message->payloadlen, stdout);
-				printf("\n");
-		}else{
-				printf("%s (null)\n", message->topic);
-		}
-		fflush(stdout);
+    zmsg_t *msg = zmsg_new ();
+    zmsg_addstr (msg, message->topic);
+    if (message->payload)
+        zmsg_addstr (msg, message->payload);
+    zmsg_send (&msg, mqtt_writter);
 }
 
-int main () {
-
+static void
+s_mosquitto_actor (zsock_t *pipe, void *args)
+{
     int r;
-
-    zsys_init ();
 
     r = mosquitto_lib_init ();
     assert (r == MOSQ_ERR_SUCCESS);
 
     zuuid_t *uuid = zuuid_new ();
+    char *endpoint = zsys_sprintf (" inproc://%s-mqtt", zuuid_str_canonical (uuid));
+    endpoint [0] = '@';
+    zsock_t *mqtt_writter = zsock_new_pair (endpoint);
+    endpoint [0] = '>';
+    zsock_t *mqtt_reader  = zsock_new_pair (endpoint);
+    zstr_free (&endpoint);
+
+    zpoller_t *poller = zpoller_new (pipe, mqtt_reader, NULL);
 
     struct mosquitto *mosq = mosquitto_new (
         zuuid_str_canonical (uuid),
         false,
-        NULL
+        (void*) mqtt_writter
     );
     assert (mosq);
     zuuid_destroy (&uuid);
@@ -59,23 +64,46 @@ int main () {
     mosquitto_connect_callback_set (mosq, s_connect);
     mosquitto_message_callback_set (mosq, s_message);
 
-    mosquitto_loop_start (mosq);
+    zsock_signal (pipe, 0);
 
+    mosquitto_loop_start (mosq);
     r = mosquitto_connect_bind_async (
         mosq,
-        "::1",
-        1883,
+        "127.0.0.1",
+        1883, 
         10,
-        "::1");
+        "127.0.0.1");
     assert (r == MOSQ_ERR_SUCCESS);
+
+    while (!zsys_interrupted)
+    {
+        void *which = zpoller_wait (poller, -1);
+        if (which == pipe)
+            break;
+
+        if (which == mqtt_reader) {
+            zmsg_t *msg = zmsg_recv (mqtt_reader);
+            zsys_debug ("=============== s_mosquitto_actor ==========================");
+            zmsg_print (msg);
+            zmsg_destroy (&msg);
+        }
+    }
+
+    mosquitto_loop_stop (mosq, true);
+
+    zsock_destroy (&mqtt_reader);
+    zsock_destroy (&mqtt_writter);
+
+    r = mosquitto_lib_cleanup ();
+    assert (r == MOSQ_ERR_SUCCESS);
+}
+
+int main () {
     
-    mosquitto_subscribe (mosq, NULL, "#", 0);
+    zactor_t *zmosq = zactor_new (s_mosquitto_actor, NULL);
 
     while (!zsys_interrupted)
         zclock_sleep (500);
 
-    mosquitto_loop_stop (mosq, true);
-
-    r = mosquitto_lib_cleanup ();
-    assert (r == MOSQ_ERR_SUCCESS);
+    zactor_destroy (&zmosq);
 }
